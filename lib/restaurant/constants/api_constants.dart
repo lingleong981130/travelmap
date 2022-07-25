511f708bf82cfede0746d808d488b9c4f303911b
lingleong981130 <72427062+lingleong981130@users.noreply.github.com>
2022-07-25 16:39:11 +0800

    Update api_constants.dart

--- lib/restaurant/constants/api_constants.dart
+++ lib/restaurant/constants/api_constants.dart
@@ -2,6 +2,6 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ApiConstants {
   static String getPlaces(LatLng input, String googleApiKey) {
-    return "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${input.latitude},${input.longitude}&rankby=distance&type=restaurant&key=AIzaSyA-odTvOMj0bYG0q6Hnzllp3qO4ZsYeeCg";
+    return "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${input.latitude},${input.longitude}&rankby=distance&type=restaurant&key=API_KEY";
   }
 }
